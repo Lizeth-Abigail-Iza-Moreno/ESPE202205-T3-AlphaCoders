b033b668108ded522f55b8b2026c74ba8f860190
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-05-31 12:07:02 -0500

    Create Factura.php

---
+++ 06-Code/TiendaOnline/views/Factura.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <table class="table table-bordered table-striped" id="details-country">
+          <tr>
+            <td>Cantidad:</td>
+             <td>Descripción:</td>
+             <td>Talla:</td>  
+             <td>Valor Unitario:</td>
+             <td>Precio Total:</td>
+             <td>Subtotal</td>
+             <td>Iva</td>
+             <td>Total</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</body>
+</html>

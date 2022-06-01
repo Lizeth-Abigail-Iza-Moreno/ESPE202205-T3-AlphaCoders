d261806669d9245c1799f61b36802c3d4f176884
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-05-31 21:36:45 -0500

    Update validarUsuario.php

--- 06-Code/TiendaOnline/controller/validarUsuario.php
+++ 06-Code/TiendaOnline/controller/validarUsuario.php
@@ -1,20 +1,21 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'],"/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/db/db.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/db/db.php";
 $usuario = $_POST['txtUsuario'];
 $contrasena = $_POST['txtContrasena'];
-$query = new MongoDB\Driver\Query(array('usuario' =>'contrasena'=> $contrasena));
+$query = new MongoDB\Driver\Query(array('usuario' => $usuario, 'contrasena'=> $contrasena));
 
 
-$cursor =$manager ->executeQuery("gestionTiendaOnline.usuario",$query);
+$cursor =$manager ->executeQuery("TiendaOnline.usuario",$query);
 
 $result=$cursor->toArray();
 
-$result = count("$users");
-if($result !=0){
+$result2 = count($result);
+
+if($result2 !=0){
     header("location:http://localhost/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/views/principal.php",TRUE,301);
 }else{
     echo "El usuario ingresado no existe";
-    header("location:http://ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/",TRUE,301);
+    header("location:http://localhost/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/",TRUE,301);
     exit();
 }
 ?>

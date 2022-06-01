b36759799c37d7bfcb0e0ffe070e084b90ec30db
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-05-31 21:36:13 -0500

    Create index.php

---
+++ 06-Code/TiendaOnline/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tienda Online</title>
+</head>
+<body>
+    <?php require_once $_SERVER['DOCUMENT_ROOT']."/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/views/login.php"; ?>
+</body>
+</html>

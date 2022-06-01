4a68095eded557874baa067a7193dcb83d79bb43
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-05-31 23:47:55 -0500

    Update insertarUsuarios.php

--- 06-Code/TiendaOnline/controller/insertarUsuarios.php
+++ 06-Code/TiendaOnline/controller/insertarUsuarios.php
@@ -1,16 +1,17 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/db/db.php";
-$cedula = $_POST['txtCedula'];
-$nombre = $_POST['txtNombre'];
-$apellido = $_POST['txtApellido'];
-$usuario = $_POST['txtUsuario'];
-$email = $_POST['txtEmail'];
-$contrasena = $_POST['txtContrasena'];
+$cedula = $_POST['cedula'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$Tipo_Usuario = $_POST['Tipo_Usuario'];
+$email = $_POST['email'];
+$contrasena = $_POST['contrasena'];
 
 $obj=array{
     'cedula'=>$cedula,
     'nombre'=>$nombre,
     'apellido'=>$apellido,
+    'Tipo_Usuario'=>$Tipo_Usuario,
     'usuario'=>$usuario,
     'email'=>$email,
     'contraseña'=>$contrasena,
@@ -19,7 +20,7 @@ $insert=new MongoDB\Driver\RulWrite();
 $insert->insert($obj);
 $result=$manager->executeRulWrite('TiendaOnline.persons',$insert);
 
-if($result->getinsertedcount()--1){
+if($result->getinsertedcount()==1){
     echo'Usuario registrado';
     }else{
     echo'error de Registro';

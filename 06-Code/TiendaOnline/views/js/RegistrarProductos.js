8a836b715c93ea4eca2f74d195096215507b280f
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-01 22:26:16 -0500

    Update RegistrarProductos.js

--- 06-Code/TiendaOnline/views/js/RegistrarProductos.js
+++ 06-Code/TiendaOnline/views/js/RegistrarProductos.js
@@ -1,18 +1,19 @@
-function registrarUsuario(){
+function registrarProducto(){
     var categoria =$('#txtCategoria').val();
-    var NombreProducto=$('#txtProducto').val;
+    var NombreProducto=$('#txtProducto').val();
     var talla=$('#txtTalla').val();
-    var precio=$('#txtPrecio').val;
-    var stock=$('#txtstock').val;
+    var precio=$('#txtPrecio').val();
+    var stock=$('#txtstock').val();
 
     var url="http://localhost/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/controller/insertarProductos.php"
-}
+
 $.ajax({
     type:"POST",
     url:url,
-    data: {'categoria': categoria,'Nombre_Producto':Nombre_Producto,'Talla':Talla,'Precio':Precio,'Stock':Stock},
+    data: {'categoria': categoria,'Nombre_Producto':NombreProducto,'Talla':talla,'Precio':precio,'Stock':stock},
     success: function (response)
     {
+        alert(response);
         if(response=='registrado'){
             $('anymodal').modal('show');          
         }else{
@@ -20,3 +21,4 @@ $.ajax({
     }
     }
 })
+}

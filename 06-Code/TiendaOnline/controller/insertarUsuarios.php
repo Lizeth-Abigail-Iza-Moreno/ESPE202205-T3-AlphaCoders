55a0b537a8c42e7396030f7f399184c0a2f79afc
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-03 00:11:55 -0500

    Update insertarUsuarios.php

--- 06-Code/TiendaOnline/controller/insertarUsuarios.php
+++ 06-Code/TiendaOnline/controller/insertarUsuarios.php
@@ -1,20 +1,21 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/db/db.php";
 $cedula = $_POST['cedula'];
-$nombre = $_POST['nombre'];
+$Nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $Tipo_Usuario = $_POST['Tipo_Usuario'];
+$usuario = $_POST['usuario'];
 $email = $_POST['email'];
 $contrasena = $_POST['contrasena'];
 
 $obj=array(
     'cedula'=>$cedula,
-    'nombre'=>$nombre,
+    'nombre'=>$Nombre,
     'apellido'=>$apellido,
     'Tipo_Usuario'=>$Tipo_Usuario,
     'usuario'=>$usuario,
     'email'=>$email,
-    'contraseña'=>$contrasena,
+    'contrasena'=>$contrasena,
 );
 $insert=new MongoDB\Driver\BulkWrite();
 $insert->insert($obj);

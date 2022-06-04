b0485acc8e06efe3a5734e84ad07b765c2614b46
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-03 22:27:34 -0500

    Update validarUsuario.php

--- 06-Code/TiendaOnline/controller/validarUsuario.php
+++ 06-Code/TiendaOnline/controller/validarUsuario.php
@@ -1,21 +1,19 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/db/db.php";
-$usuario = $_POST['txtUsuario'];
-$contrasena = $_POST['txtContrasena'];
-$query = new MongoDB\Driver\Query(array('usuario' => $usuario, 'contrasena'=> $contrasena));
+$usuario = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
+$query = new MongoDB\Driver\Query(array('username' => $usuario, 'password'=> $contrasena));
 
 
-$cursor =$manager ->executeQuery("TiendaOnline.usuario",$query);
+$cursor =$manager ->executeQuery("TiendaOnline.users",$query);
 
 $result=$cursor->toArray();
 
 $result2 = count($result);
 
 if($result2 !=0){
-    header("location:http://localhost/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/principal.php",TRUE,301);
+    echo 1;
 }else{
-    alert('El usuario ingresado no existe');
-    header("location:http://localhost/ESPE202205-T3-AlphaCoders/06-Code/TiendaOnline/",TRUE,301);
-    exit();
+   echo 2;
 }
 ?>

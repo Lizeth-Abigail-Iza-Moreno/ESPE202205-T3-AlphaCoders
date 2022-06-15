85d50eca531b3e04bcdaf1501360ca0a6bfed491
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-15 14:23:08 -0500

    Update server.js

--- 06-Code/02ServiciosWeb/server.js
+++ 06-Code/02ServiciosWeb/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const portParameter = 8081
-//const EndPoints = require('./api/EndPoints')
+const EndPoints = require('./api/EndPoints')
 
 var app = express()
 

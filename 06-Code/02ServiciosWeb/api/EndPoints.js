c14724348a4a43a9077db58eaf6dcae624e13193
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-15 16:23:04 -0500

    Update EndPoints.js

--- 06-Code/02ServiciosWeb/api/EndPoints.js
+++ 06-Code/02ServiciosWeb/api/EndPoints.js
@@ -1,14 +1,15 @@
-//const ProductController = require('../controller/Products')
+const ProductController = require('../controller/Products')
 const PersonController = require('../controller/Persons')
 const express = require('express')
 const router = express.Router()
 
 //router.metodo_CRUD('uri), controlador.metodo)
-//router.post('/addProducto', ProductController.createProducto)
-//router.get('/Productos', ProductController.getProductos)
-//router.put('/updatePrecio', ProductController.updatePrecio)
-//router.delete('/deleteProducto', ProductController.deleteProducto)
+router.post('/addProducto', ProductController.createProducto)
+router.get('/Productos', ProductController.getProductos)
+router.put('/updatePrecio', ProductController.updatePrecio)
+router.delete('/deleteProducto', ProductController.deleteProducto)
 
+//CRUD Persons
 router.post('/addPersonas', PersonController.createPersona)
 router.get('/Personas', PersonController.getPersonas)
 router.put('/updateEmail', PersonController.updateemail)

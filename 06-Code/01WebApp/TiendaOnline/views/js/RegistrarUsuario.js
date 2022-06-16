e04c28527e63eded58a47f4ce743b3524df917e4
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-16 15:08:17 -0500

    Create RegistrarUsuario.js

---
+++ 06-Code/01WebApp/TiendaOnline/views/js/RegistrarUsuario.js
@@ -0,0 +1,30 @@
+function registrarUsuario(){
+    var cedula =$('#txtCedula').val();
+    var nombre=$('#txtNombre').val();
+    var apellido=$('#txtApellido').val();
+    var Tipo_Usuario=$('#txtTipo_Usuario').val();
+    var usuario=$('#txtUsuario').val();
+    var email=$('#txtEmail').val();
+    var contrasena=$('#txtContrasena').val();
+    
+    var url = "http://localhost/ESPE202205-T3-AlphaCoders/06-Code/01WebApp/TiendaOnline/controller/insertarUsuarios.php"
+
+$.ajax({
+    type:"POST",
+    url: url,
+    data: {'cedula':cedula,'nombre':nombre,'apellido':apellido,'Tipo_Usuario':Tipo_Usuario,'usuario':usuario,
+    'email':email,'contrasena':contrasena},
+    success: function (response)
+    {
+        if(response=='registrado'){
+            $('anymodal').modal('show');  
+            console.log(response) 
+            alert('registrado con exito');       
+        }else{
+            console.log(response)
+             alert('Error de registro');
+    }
+    }
+    
+})
+}

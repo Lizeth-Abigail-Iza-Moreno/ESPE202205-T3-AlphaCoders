635b2d9acc6f89a7855fa3f91dce0826d7ac9177
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-16 15:04:27 -0500

    Create ValidarUsuario.js

---
+++ 06-Code/01WebApp/TiendaOnline/views/js/ValidarUsuario.js
@@ -0,0 +1,24 @@
+function validarUsuario(){
+    var usuario=$('#txtUsuario').val();
+    var contrasena=$('#txtContrasena').val();
+     
+
+$.ajax({
+    type:"POST",
+    url: "http://localhost/ESPE202205-T3-AlphaCoders/06-Code/01WebApp/TiendaOnline/controller/validarUsuario.php",
+    data: {'usuario':usuario,'contrasena':contrasena},
+    success: function (response)
+    {
+       console.log(response);
+        if(response!=1){
+           alert('error')     
+           
+        }else{
+           alert('Welcome')
+           window.location="http://localhost/ESPE202205-T3-AlphaCoders/06-Code/01WebApp/TiendaOnline/views/principal.php";
+     
+    }
+    }
+    
+})
+}

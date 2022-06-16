ba446b1cbbfb1afed826201c40ea790436511fe0
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-06-16 15:07:14 -0500

    Create validarUsuario.php

---
+++ 06-Code/01WebApp/TiendaOnline/controller/validarUsuario.php
@@ -0,0 +1,19 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']."/ESPE202205-T3-AlphaCoders/06-Code/01WebApp/TiendaOnline/db/db.php";
+$usuario = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
+$query = new MongoDB\Driver\Query(array('username' => $usuario, 'password'=> $contrasena));
+
+
+$cursor =$manager ->executeQuery("TiendaOnline.users",$query);
+
+$result=$cursor->toArray();
+
+$result2 = count($result);
+
+if($result2 !=0){
+    echo 1;
+}else{
+   echo 2;
+}
+?>

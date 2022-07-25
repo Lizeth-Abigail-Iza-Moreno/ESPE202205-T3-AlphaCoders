52dd60cde5cfd9f91c791c2729345347fae486f9
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-07-25 18:58:00 -0500

    Update products.js

--- 06-Code/03WebClient/app_onlinestore/src/pages/products.js
+++ 06-Code/03WebClient/app_onlinestore/src/pages/products.js
@@ -1,6 +1,7 @@
 import React, { Component, useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import Menu from '../pages/menu'
 
 export default class Users extends Component {  
     render() {
@@ -15,32 +16,32 @@ export default class Users extends Component {
                 <form class="form-card" >
                   <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Categoria<span class="text-danger"> *</span></label> <input type="text" id="txtCedula" name="txtCedula" onChange={this.handleChange} />
+                      Categoria<span class="text-danger"> *</span></label> <input type="text" id="txtCategoria" name="txtCategoria" onChange={this.handleChange} />
                     </div>             
                     <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Nombre Producto<span class="text-danger"> *</span></label> <input type="text" id="txtNombre" name="txtNombre"  onChange={this.handleChange} /> 
+                      Nombre Producto<span class="text-danger"> *</span></label> <input type="text" id="txtNombreProducto" name="NombreProducto"  onChange={this.handleChange} /> 
                     </div>
                     </div>
                     <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Talla<span class="text-danger"> *</span></label> <input type="text" id="txtApellido" name="txtApellido"  onChange={this.handleChange} /> 
+                      Talla<span class="text-danger"> *</span></label> <input type="text" id="txtTalla" name="txtTalla"  onChange={this.handleChange} /> 
                       </div>
                       </div>
                     <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Precio<span class="text-danger"> *</span></label> <input type="text" id="txtTipo" name="txtTipo"  onChange={this.handleChange} /> 
+                      Precio<span class="text-danger"> *</span></label> <input type="text" id="txtPrecio" name="txtPrecio"  onChange={this.handleChange} /> 
                       </div>
                       </div>
                       <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                    Stock:<span class="text-danger"> *</span></label> <input type="text" id="txtUsuario" name="txtUsuario" onChange={this.handleChange} />
+                    Stock:<span class="text-danger"> *</span></label> <input type="text" id="txtStock" name="txtStock" onChange={this.handleChange} />
                     </div>
                   </div>
                   </div>
                   <br></br>
                   <div class="row justify-content-center">
-                    <div class="form-group col-sm-6"> <button onClick={() => this.putStudent()} type="submit" class="btn-block btn-primary">Registrar</button> </div>
+                    <div class="form-group col-sm-6"> <button  type="submit" class="btn-block btn-primary">Registrar</button> </div>
                   </div>
                 </form>
               </div>

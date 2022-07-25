1e64f274f2490c1c53bc7c1fdce9fa5222d784e8
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-07-25 17:30:19 -0500

    Update users.js

--- 06-Code/03WebClient/app_onlinestore/src/pages/users.js
+++ 06-Code/03WebClient/app_onlinestore/src/pages/users.js
@@ -1,9 +1,61 @@
-import React, { Component } from 'react'
-export default class users extends Component {
-  render() {
+import React, { Component, useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+export default class Users extends Component {  
+    render() {
     return (
-        
-      <div>users</div>
+      <div>
+        <div class="container-fluid px-1 py-5 mx-auto">
+          <div class="row d-flex justify-content-center">
+            <div class="col-xl-6 col-lg-8 ">
+              <h3>REGISTRO DE USUARIOS</h3>
+              <div class="card">
+
+                <form class="form-card" >
+                  <div class="row justify-content-center">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Cédula<span class="text-danger"> *</span></label> <input type="text" id="txtCedula" name="txtCedula" onChange={this.handleChange} />
+                    </div>             
+                    <div class="row justify-content-center">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Nombres<span class="text-danger"> *</span></label> <input type="text" id="txtNombre" name="txtNombre"  onChange={this.handleChange} /> 
+                    </div>
+                    </div>
+                    <div class="row justify-content-center">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Apellidos<span class="text-danger"> *</span></label> <input type="text" id="txtApellido" name="txtApellido"  onChange={this.handleChange} /> 
+                      </div>
+                      </div>
+                    <div class="row justify-content-center">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Tipo de usuario<span class="text-danger"> *</span></label> <input type="text" id="txtTipo" name="txtTipo"  onChange={this.handleChange} /> 
+                      </div>
+                      </div>
+                      <div class="row justify-content-center">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Usuario<span class="text-danger"> *</span></label> <input type="text" id="txtUsuario" name="txtUsuario" onChange={this.handleChange} />
+                    </div>
+                  </div>
+                  <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Email<span class="text-danger"> *</span></label> <input type="text" id="txtEmail" name="txtEmail" onChange={this.handleChange} />
+                    </div>
+                  </div>
+                  <div class="row justify-content-center">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Contraseña<span class="text-danger"> *</span></label> <input type="text" id="txtPassword" name="txtPassword" onChange={this.handleChange} />
+                    </div>
+                  </div>
+                  <br></br>
+                  <div class="row justify-content-center">
+                    <div class="form-group col-sm-6"> <button onClick={() => this.putStudent()} type="submit" class="btn-block btn-primary">Registrar</button> </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }

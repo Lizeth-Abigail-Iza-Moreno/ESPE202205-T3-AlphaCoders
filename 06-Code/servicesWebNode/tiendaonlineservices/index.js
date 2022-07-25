8497f2c5bc616662dba94f0ad570c09d6c0b96dc
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-07-24 21:35:16 -0500

    Create index.js

---
+++ 06-Code/servicesWebNode/tiendaonlineservices/index.js
@@ -0,0 +1,19 @@
+const port = 3003
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(`mongodb+srv://administrador:administrador@cluster0.hlyms.mongodb.net/TiendaOnline?retryWrites=true&w=majority`, { useNewUrlParser: true })
+
+const db = mongoose.connection;
+
+db.on("error", (error) => crossOriginIsolated.error(error));
+db.once("open", () => console.log("System connected to MongoDB Database"));
+
+app.use(express.json());
+
+const productRouter = require("./routes/productsRoutes");
+
+app.use("/onlinestore", productRouter);
+
+app.listen(port, () => console.log('Server is running  on port --> ' + port));

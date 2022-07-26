a4996bbc6de615a77d909c63179a03b1b8ba9bdb
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-07-25 23:51:56 -0500

    Update App.js

--- 06-Code/03WebClient/app_onlinestore/src/routes/App.js
+++ 06-Code/03WebClient/app_onlinestore/src/routes/App.js
@@ -1,26 +1,20 @@
-import '../css/App.css';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from '../pages/login';
-import Menu from '../pages/menu';
+import Menu from '../pages/menu'
 import Products from '../pages/products';
 import Users from '../pages/users';
-import Journal from '../pages/journal';
-
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-        <Route path='/' element={<Login />} />
-          <Route path='/menu' element={<Menu />} />
-          <Route path='products' element={<Products />} />
-          <Route path='users' element={<Users />} />
-          <Route path='journal' element={<Journal />} />
-          <Route path='*' element={<Navigate replace to="/" />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Login/>} />
+        <Route exact path='/menu' element={<Menu/>}/>
+        <Route exact path='/users' element={<Users/>}/> 
+        <Route exact path='/products' element={<Products/>}/> 
+        
+      </Routes>
+    </Router>
   );
 }
 export default App;

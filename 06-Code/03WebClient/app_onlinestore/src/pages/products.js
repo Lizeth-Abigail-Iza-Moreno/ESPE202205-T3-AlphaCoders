7bf328f4496e845d7d20d6c2e04edabfcb1f9f99
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-07-25 23:51:47 -0500

    Update products.js

--- 06-Code/03WebClient/app_onlinestore/src/pages/products.js
+++ 06-Code/03WebClient/app_onlinestore/src/pages/products.js
@@ -1,12 +1,15 @@
 import React, { Component, useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import ProductsSearch from '../components/ProductsSearch';
 import Menu from '../pages/menu'
 
-export default class Users extends Component {  
-    render() {
+export default class Users extends Component {
+  render() {
     return (
       <div>
+        <Menu/>
+        <ProductsSearch />
         <div class="container-fluid px-1 py-5 mx-auto">
           <div class="row d-flex justify-content-center">
             <div class="col-xl-4 col-lg-8 ">
@@ -17,31 +20,31 @@ export default class Users extends Component {
                   <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
                       Categoria<span class="text-danger"> *</span></label> <input type="text" id="txtCategoria" name="txtCategoria" onChange={this.handleChange} />
-                    </div>             
-                    <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Nombre Producto<span class="text-danger"> *</span></label> <input type="text" id="txtNombreProducto" name="NombreProducto"  onChange={this.handleChange} /> 
-                    </div>
                     </div>
                     <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Talla<span class="text-danger"> *</span></label> <input type="text" id="txtTalla" name="txtTalla"  onChange={this.handleChange} /> 
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Nombre Producto<span class="text-danger"> *</span></label> <input type="text" id="txtNombreProducto" name="NombreProducto" onChange={this.handleChange} />
                       </div>
-                      </div>
-                    <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Precio<span class="text-danger"> *</span></label> <input type="text" id="txtPrecio" name="txtPrecio"  onChange={this.handleChange} /> 
-                      </div>
-                      </div>
-                      <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                    Stock:<span class="text-danger"> *</span></label> <input type="text" id="txtStock" name="txtStock" onChange={this.handleChange} />
                     </div>
-                  </div>
+                    <div class="row justify-content-center">
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Talla<span class="text-danger"> *</span></label> <input type="number" id="txtTalla" name="txtTalla" />
+                      </div>
+                    </div>
+                    <div class="row justify-content-center">
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Precio<span class="text-danger"> *</span></label> <input type="number" id="txtPrecio" name="txtPrecio" />
+                      </div>
+                    </div>
+                    <div class="row justify-content-center">
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Stock:<span class="text-danger"> *</span></label> <input type="number" id="txtStock" name="txtStock" />
+                      </div>
+                    </div>
                   </div>
                   <br></br>
                   <div class="row justify-content-center">
-                    <div class="form-group col-sm-6"> <button  type="submit" class="btn-block btn-primary">Registrar</button> </div>
+                    <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Registrar</button> </div>
                   </div>
                 </form>
               </div>

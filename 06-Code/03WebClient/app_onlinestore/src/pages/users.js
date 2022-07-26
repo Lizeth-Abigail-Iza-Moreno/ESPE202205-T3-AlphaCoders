ebbbe57b34d8d6378c57e7e3ebe73623d1167f11
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-07-25 23:51:51 -0500

    Update users.js

--- 06-Code/03WebClient/app_onlinestore/src/pages/users.js
+++ 06-Code/03WebClient/app_onlinestore/src/pages/users.js
@@ -3,10 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Menu from '../pages/menu'
 
-export default class Users extends Component {  
-    render() {
+export default class Users extends Component {
+  render() {
     return (
       <div>
+        <Menu />
         <div class="container-fluid px-1 py-5 mx-auto">
           <div class="row d-flex justify-content-center">
             <div class="col-xl-6 col-lg-8 ">
@@ -17,34 +18,34 @@ export default class Users extends Component {
                   <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
                       Cédula<span class="text-danger"> *</span></label> <input type="text" id="txtCedula" name="txtCedula" onChange={this.handleChange} />
-                    </div>             
-                    <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Nombres<span class="text-danger"> *</span></label> <input type="text" id="txtNombre" name="txtNombre"  onChange={this.handleChange} /> 
-                    </div>
                     </div>
                     <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Apellidos<span class="text-danger"> *</span></label> <input type="text" id="txtApellido" name="txtApellido"  onChange={this.handleChange} /> 
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Nombres<span class="text-danger"> *</span></label> <input type="text" id="txtNombre" name="txtNombre" onChange={this.handleChange} />
                       </div>
-                      </div>
-                    <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Tipo de usuario<span class="text-danger"> *</span></label> <input type="text" id="txtTipo" name="txtTipo"  onChange={this.handleChange} /> 
-                      </div>
-                      </div>
-                      <div class="row justify-content-center">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Usuario<span class="text-danger"> *</span></label> <input type="text" id="txtUsuario" name="txtUsuario" onChange={this.handleChange} />
                     </div>
-                  </div>
-                  <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Email<span class="text-danger"> *</span></label> <input type="text" id="txtEmail" name="txtEmail" onChange={this.handleChange} />
+                    <div class="row justify-content-center">
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Apellidos<span class="text-danger"> *</span></label> <input type="text" id="txtApellido" name="txtApellido" onChange={this.handleChange} />
+                      </div>
+                    </div>
+                    <div class="row justify-content-center">
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Tipo de usuario<span class="text-danger"> *</span></label> <input type="text" id="txtTipo" name="txtTipo" />
+                      </div>
+                    </div>
+                    <div class="row justify-content-center">
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                        Usuario<span class="text-danger"> *</span></label> <input type="text" id="txtUsuario" name="txtUsuario" />
+                      </div>
+                    </div>
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Email<span class="text-danger"> *</span></label> <input type="text" id="txtEmail" name="txtEmail" />
                     </div>
                   </div>
                   <div class="row justify-content-center">
                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
-                      Contraseña<span class="text-danger"> *</span></label> <input type="text" id="txtPassword" name="txtPassword" onChange={this.handleChange} />
+                      Contraseña<span class="text-danger"> *</span></label> <input type="text" id="txtPassword" name="txtPassword" />
                     </div>
                   </div>
                   <br></br>

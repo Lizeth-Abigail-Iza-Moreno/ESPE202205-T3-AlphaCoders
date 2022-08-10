a2a4dfe2f6566725d494713dc06de3e5046982b7
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-08-10 13:40:17 -0500

    Update index.js

--- 06-Code/servicesWebNode/tiendaonlineservices/index.js
+++ 06-Code/servicesWebNode/tiendaonlineservices/index.js
@@ -13,7 +13,8 @@ db.once("open", () => console.log("System connected to MongoDB Database"));
 app.use(express.json());
 
 const productRouter = require("./routes/productsRoutes");
-
+const personRouter = require("./routes/personsRoutes");
 app.use("/onlinestore", productRouter);
+app.use("/onlinestore", personRouter);
 
 app.listen(port, () => console.log('Server is running  on port --> ' + port));

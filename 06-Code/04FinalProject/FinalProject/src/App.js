73b2cb88a06bddff6537500eadb0e6feecbaecb6
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2022-08-10 16:11:55 -0500

    Create App.js

---
+++ 06-Code/04FinalProject/FinalProject/src/App.js
@@ -0,0 +1,198 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import Button from '@mui/material/Button';
+
+const URI = 'http://3.86.206.55:3003/onlinestore/products';
+
+class Product extends Component {
+  state = {
+    data: [],
+    modalInsert: false,
+    modalDelete: false,
+    form: {
+      _id: '',
+      category: '',
+      product_Name: '',
+      price: '',
+      size: '',
+      stock: '',
+    }
+  }
+
+  callGet = () => {
+    axios.get(URI).then(response => {
+      this.setState({ data: response.data });
+    }).catch(error => {
+      console.log(error.message);
+    })
+  }
+
+  callPost = async () => {
+    delete this.state.form._id;
+    await axios.post(URI, this.state.form).then(response => {
+      this.modalInsert();
+      this.callGet();
+    }).catch(error => {
+      console.log(error.message);
+    })
+  }
+
+  callPut = () => {
+    axios.put(URI + this.state.form._id, this.state.form).then(response => {
+      this.modalInsert();
+      this.callGet();
+    })
+  }
+
+  callDelete = () => {
+    axios.delete(URI + this.state.form._id).then(response => {
+      this.setState({ modalDelete: false });
+      this.callGet();
+    })
+  }
+
+  modalInsert = () => {
+    this.setState({ modalInsert: !this.state.modalInsert });
+  }
+
+  selectProduct = (product) => {
+    this.setState({
+      tipoModal: 'actualizar',
+      form: {
+        _id: product._id,
+        category: product.category,
+        product_Name: product.product_Name,
+        price: product.price,
+        size: product.size,
+        stock: product.stock,
+      }
+    })
+  }
+
+  captureData = async data => {
+    data.persist();
+    await this.setState({
+      form: {
+        ...this.state.form,
+        [data.target.name]: data.target.value
+      }
+    });
+    console.log(this.state.form);
+  }
+
+  componentDidMount() {
+    this.callGet();
+  }
+
+
+  render() {
+    const { form } = this.state;
+    return (
+      <div className="App">
+        <div class="table-responsive-md">
+          <table className="table ">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Category</th>
+                <th>Product Name</th>
+                <th>price</th>
+                <th>size</th>
+                <th>Stock</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.data.map(product => {
+                return (
+                  <tr>
+                    <td>{product._id}</td>
+                    <td>{product.category}</td>
+                    <td>{product.product_Name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.size}</td>
+                    <td>{product.stock}</td>
+
+                    <td>
+                      <button className="btn btn-primary" onClick={() => { this.selectClient(product); this.modalInsert() }}><FontAwesomeIcon icon={faEdit} /></button>
+                      {"   "}
+                      <button className="btn btn-danger" onClick={() => { this.selectClient(product); this.setState({ modalDelete: true }) }}><FontAwesomeIcon icon={faTrashAlt} /></button>
+                    </td>
+                  </tr>
+
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        <Modal isOpen={this.state.modalInsert}>
+          <ModalHeader style={{ display: 'block' }}>
+            <span style={{ float: 'right' }} onClick={() => this.modalInsert()}>x</span>
+          </ModalHeader>
+          <ModalBody>
+
+            <div className="form-group">
+              <label htmlFor="_id">ID</label>
+              <input className="form-control" type="text" name="id" id="id" readOnly onChange={this.captureData} value={form ? form._id : this.state.data.length + 1} />
+              <br />
+              <label htmlFor="Name">Category</label>
+              <input className="form-control" type="text" name="category" id="category" onChange={this.captureData} value={form ? form.category : ''} />
+              <br />
+              <label htmlFor="LastName">Product Name</label>
+              <input className="form-control" type="text" name="product_Name" id="product_Name" onChange={this.captureData} value={form ? form.product_Name : ''} />
+              <br />
+              <label htmlFor="age">Price</label>
+              <input className="form-control" type="text" name="price" id="price" onChange={this.captureData} value={form ? form.price : ''} />
+              <br />
+              <label htmlFor="subTotal">Size</label>
+              <input className="form-control" type="text" name="size" id="size" onChange={this.captureData} value={form ? form.size : ''} />
+              <br />
+              <label htmlFor="age">Stock</label>
+              <input className="form-control" type="text" name="stock" id="stock" onChange={this.captureData} value={form ? form.stock : ''} />
+              <br />
+            </div>
+          </ModalBody>
+
+          <ModalFooter>
+            {this.state.tipoModal === 'insertar' ?
+              <button className="btn btn-success" onClick={() => this.callPost()}>
+                Insertar
+              </button> : <button className="btn btn-primary" onClick={() => this.callPut()}>
+                Actualizar
+              </button>
+            }
+            <button className="btn btn-danger" onClick={() => this.modalInsert()}>Cancelar</button>
+          </ModalFooter>
+        </Modal>
+
+
+        <Modal isOpen={this.state.modalDelete}>
+          <ModalBody>
+            Estás seguro que deseas eliminar el product: {form && form._id}
+          </ModalBody>
+          <ModalFooter>
+            <button className="btn btn-danger" onClick={() => this.callDelete()}>Sí</button>
+            <button className="btn btn-secundary" onClick={() => this.setState({ modalDelete: false })}>No</button>
+          </ModalFooter>
+        </Modal>
+        <Button
+          className='login'
+          fullWidth
+          variant="contained"
+          style={{ color: 'white', backgroundColor: "#5DC1B9" }}
+          onClick={() => { this.setState({ form: null, tipoModal: 'insertar' }); this.modalInsert() }}
+        >
+          <strong><p>Add product</p></strong>
+        </Button>
+      </div>
+
+
+
+    );
+  }
+}
+export default Product;
